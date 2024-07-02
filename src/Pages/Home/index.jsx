6c51b944c2dcaf7ccd67e0a6f4941d5d848cb609
Aryan Sethi <aryan.rdps@gmail.com>
2024-07-02 22:52:11 +0530

    Added country card with css added

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Home.css";
 import FilterSearch from "../../Components/FilterSearch";
+import CountryCard from "../../Components/CountryCard";
+import CountryContext from "../../Context/Country/CountryContext";
 
 const Home = () => {
+    const { countries } = useContext(CountryContext);
+    const temp_count = countries.slice(0, 9);
     return (
         <div className="container">
             <FilterSearch />
+
+            <div className="cards-grid">
+                {countries.length > 0 &&
+                    temp_count.map(country => (
+                        <CountryCard
+                            key={country?.name?.common}
+                            country={country}
+                        />
+                    ))}
+            </div>
         </div>
     );
 };
