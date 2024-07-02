892ce228634eceb3d7916bada3862b2343f9746a
Aryan Sethi <aryan.rdps@gmail.com>
2024-07-02 14:13:36 +0530

    added nav designs for dark and light with theme switcher

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Navbar.css";
+import ThemeContext from "../../Context/Theme/ThemeContext";
 
 const Navbar = () => {
-    return <div>Navbar</div>;
+    const { setTheme } = useContext(ThemeContext);
+
+    return (
+        <div className="nav-contain">
+            <div className="navbox container">
+                <h1>Where in the world?</h1>
+
+                <div
+                    className="theme-switch"
+                    onClick={e => {
+                        setTheme(prev => (prev === "light" ? "dark" : "light"));
+                    }}
+                >
+                    <div className="icon"></div>
+                    <span>Dark Mode</span>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Navbar;
